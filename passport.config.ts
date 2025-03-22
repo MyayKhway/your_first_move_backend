8f7f817486d3719988923249608e23b3a7792195
KentNyiLynn <kentnyilynn@proton.me>
2025-03-22 16:11:33 +0700

    prepare for deployment

--- passport.config.ts
+++ passport.config.ts
@@ -2,9 +2,9 @@ const jwtOpts = {
   secret: process.env.SECRET_KEY || "SeCreEt",
   cookieName: "auth_token",
   options: {
-    audience: 'http://localhost:5173',
+    audience: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : 'http://localhost:5173',
     expiresIn: '7d',
-    issuer: 'localhost:3000',
+    issuer: process.env.NODE_ENV === "production" ? 'your-first-move-44b7e461.nip.io' : 'localhost:3000',
   },
   cookie: {
     httpOnly: true,
